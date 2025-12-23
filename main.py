21e7d57a931f1cfe6a6e38f03198323967aeaf07
Don Kukral <dkukral@gmail.com>
2025-12-23 14:14:16 -0500

    Refactor main.py to enhance MCP functionality and integrate LangChain tools
    - Updated main.py to include HumanMessage from langchain_core.messages for message handling.
    - Modified the import path for load_mcp_tools to reflect the new structure.
    - Enhanced the main function to initialize the MCP session, load tools, and invoke a sample agent with a math query.

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from langchain_mcp_adapters import load_mcp_tools
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
 
 load_dotenv()
@@ -17,9 +18,19 @@ stdio_server_params = StdioServerParameters(
     command="python", args=[r"C:\Users\dkukr\projects\mcp-servers\mcp-crash-course\servers\math_server.py"],
 )
 
+
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+            
+            agent = create_react_agent(llm, tools)
 
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
+            
 if __name__ == "__main__":
     asyncio.run(main())
